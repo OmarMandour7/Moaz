a8697fecc1c591d5eaad72fa1e63170b2a6990e1
Your Name <you@example.com>
2025-09-26 07:07:32 +0300

    trafick

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,39 @@
+// app/layout.jsx
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 export const metadata = {
-  title: "Moaz Portfolio",
-  description: "Moaz Ali",
+    title: "Moaz Portfolio",
+    description: "Moaz Ali",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+
+    return (
+        <html lang="en">
+        <head>
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=G-JSFE0F3LMN`}
+                strategy="afterInteractive"
+            />
+            <Script id="ga-config" strategy="afterInteractive">
+                {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JSFE0F3LMN', {
+              page_path: window.location.pathname,
+            });
+          `}
+            </Script>
+        </head>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
