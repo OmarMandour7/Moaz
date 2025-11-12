30783a6dc8dcc90f18e1a4320244fdf06c68cdf7
Your Name <you@example.com>
2025-11-12 03:10:47 +0200

    update 3lwan

--- app/(site)/Project/page.js
+++ app/(site)/Project/page.js
@@ -6,139 +6,225 @@ export default function Home() {
     const projects = [
         {
             id: 1,
-            title: 'Ebda3 Gate',
-            image: '/Images/Ebda3Gate1.jpg',  hight : '350px'
+            title: 'Eng/Mohamed Elwan',
+            image: '/Images/Elwan1.jpg',
+            height: '250px'
         },
         {
             id: 2,
-            title: 'Ebda3 Gate',
-            image: '/Images/Ebda3Gate2.jpg',  hight : '350px'
+            title: 'Eng/Mohamed Elwan',
+            image: '/Images/Elwan2.jpg',
+            height: '250px'
         },
         {
             id: 3,
-            title: 'Ebda3 Gate',
-            image: '/Images/Ebda3Gate3.jpg',  hight : '350px'
+            title: 'Eng/Mohamed Elwan',
+            image: '/Images/Elwan3.jpg',
+            height: '250px'
         },
         {
             id: 4,
-            title: 'UNI Line ',
-            image: '/Images/ULine4.jpg',
-            hight : '350px'
-
+            title: 'LAFORTUNE',
+            image: '/Images/LAFORTUNE1.jpg',
+            height: '400px'
         },
         {
             id: 5,
-            title: 'UNI Line ',
-            image: '/Images/ULine2.jpg' ,
-            hight : '350px'
+            title: 'LAFORTUNE',
+            image: '/Images/LAFORTUNE2.jpg',
+            height: '400px'
         },
         {
             id: 6,
-            title: 'UNI Line ',
-            image: '/Images/ULine3.jpg',  hight : '350px'
+            title: 'LAFORTUNE',
+            image: '/Images/LAFORTUNE3.jpg',
+            height: '400px'
         },
-
         {
             id: 7,
-            title: 'UNI Line ',
-            image: '/Images/ULine.jpg',
-            hight : '350px'
-
+            title: 'Ebda3 Gate',
+            image: '/Images/Ebda3Gate4.jpg',
+            height: '350px'
         },
         {
             id: 8,
-            title: 'UNI Line ',
-            image: '/Images/ULine5.png' ,
-            hight : '350px'
+            title: 'Ebda3 Gate',
+            image: '/Images/Ebda3Gate5.jpg',
+            height: '350px'
         },
         {
             id: 9,
-            title: 'UNI Line ',
-            image: '/Images/ULine6.jpg',  hight : '350px'
+            title: 'Ebda3 Gate',
+            image: '/Images/Ebda3Gate6.jpg',
+            height: '350px'
         },
         {
             id: 10,
-            title: 'Smash In ',
-            image: '/Images/Smash1.jpg',
-            hight : '450px'
-
+            title: 'Ebda3 Gate',
+            image: '/Images/Ebda3Gate7.jpg',
+            height: '350px'
         },
         {
             id: 11,
-            title: 'Smash In ',
-            image: '/Images/Smash2.jpg',
-            hight : '400px'
+            title: 'Ebda3 Gate',
+            image: '/Images/Ebda3Gate8.jpg',
+            height: '350px'
         },
         {
             id: 12,
-            title: ' Grand Shawrma ',
-            image: '/Images/Grand.jpg',  hight : '450px'
+            title: 'Ebda3 Gate',
+            image: '/Images/Ebda3Gate9.jpg',
+            height: '350px'
         },
         {
             id: 13,
-            title: 'Couch Ali ',
-            image: '/Images/ALI1.png',  hight : '650px'
+            title: 'Ebda3 Gate',
+            image: '/Images/Ebda3Gate1.jpg',
+            height: '350px'
         },
         {
             id: 14,
-            title: 'Couch Ali ',
-            image: '/Images/ALI2.png',  hight : '650px'
+            title: 'Ebda3 Gate',
+            image: '/Images/Ebda3Gate2.jpg',
+            height: '350px'
         },
         {
             id: 15,
-            title: ' Couch Ali ',
-            image: '/Images/ALI3.png',  hight : '650px'
+            title: 'Ebda3 Gate',
+            image: '/Images/Ebda3Gate3.jpg',
+            height: '350px'
         },
         {
             id: 16,
-            title: 'Couch Khaled ',
-            image: '/Images/Khaled.jpg',  hight : '650px'
+            title: 'UNI Line ',
+            image: '/Images/ULine4.jpg',
+            height: '350px'
         },
         {
             id: 17,
-            title: 'Couch Khaled ',
-            image: '/Images/Khaled3.jpg',  hight : '600px'
+            title: 'UNI Line ',
+            image: '/Images/ULine2.jpg',
+            height: '350px'
         },
         {
             id: 18,
-            title: ' Couch Khaled ',
-            image: '/Images/Khaled2.png',  hight : '650px'
+            title: 'UNI Line ',
+            image: '/Images/ULine3.jpg',
+            height: '350px'
         },
         {
             id: 19,
-            title: ' EVOLVE ',
-            image: '/Images/EVOLVE.png',  hight : '350px'
+            title: 'UNI Line ',
+            image: '/Images/ULine.jpg',
+            height: '350px'
         },
         {
             id: 20,
-            title: ' PLAN88 ',
-            image: '/Images/Hiring.jpg',  hight : '350px'
+            title: 'UNI Line ',
+            image: '/Images/ULine5.png',
+            height: '350px'
         },
         {
             id: 21,
-            title: ' Eldawly ',
-            image: '/Images/Dawly.jpg',  hight : '350px'
-        }
-        ,
+            title: 'UNI Line ',
+            image: '/Images/ULine66.jpg',
+            height: '350px'
+        },
         {
             id: 22,
-            title: ' Double Dare ',
-            image: '/Images/Double.png',  hight : '320px'
-        }
-        ,
+            title: 'Smash In ',
+            image: '/Images/Smash1.jpg',
+            height: '450px'
+        },
         {
             id: 23,
-            title: ' Perfume ',
-            image: '/Images/Pefume.jpg',  hight : '300px'
-        }
-        ,
+            title: 'Smash In ',
+            image: '/Images/Smash2.jpg',
+            height: '400px'
+        },
         {
             id: 24,
+            title: 'Smash In ',
+            image: '/Images/Smash3.jpg',
+            height: '450px'
+        },
+        {
+            id: 25,
+            title: 'Couch Ali ',
+            image: '/Images/ALI1.png',
+            height: '650px'
+        },
+        {
+            id: 26,
+            title: 'Couch Ali ',
+            image: '/Images/ALI2.png',
+            height: '650px'
+        },
+        {
+            id: 27,
+            title: ' Couch Ali ',
+            image: '/Images/ALI3.png',
+            height: '650px'
+        },
+        {
+            id: 28,
+            title: 'Couch Khaled ',
+            image: '/Images/Khaled.jpg',
+            height: '650px'
+        },
+        {
+            id: 29,
+            title: 'Couch Khaled ',
+            image: '/Images/Khaled3.jpg',
+            height: '600px'
+        },
+        {
+            id: 30,
+            title: ' Couch Khaled ',
+            image: '/Images/Khaled2.png',
+            height: '650px'
+        },
+        {
+            id: 31,
+            title: ' EVOLVE ',
+            image: '/Images/EVOLVE.png',
+            height: '350px'
+        },
+        {
+            id: 32,
+            title: ' PLAN88 ',
+            image: '/Images/Hiring.jpg',
+            height: '350px'
+        },
+        {
+            id: 33,
+            title: ' Eldawly ',
+            image: '/Images/Dawly.jpg',
+            height: '350px'
+        },
+        {
+            id: 34,
+            title: ' Double Dare ',
+            image: '/Images/Double.png',
+            height: '320px'
+        },
+        {
+            id: 35,
+            title: ' Perfume ',
+            image: '/Images/Pefume.jpg',
+            height: '300px'
+        },
+        {
+            id: 36,
             title: ' Twist ',
-            image: '/Images/Twist.png',  hight : '320px'
+            image: '/Images/Twist.png',
+            height: '320px'
         }
     ];
-  return (
+
+
+
+    return (
       <section id="projects" className="py-12 sm:py-16 lg:py-20 px-4 sm:px-6 lg:px-8 ">
           <div className="max-w-7xl mx-auto">
               {/* Section Header */}
@@ -156,7 +242,7 @@ export default function Home() {
                   {projects.map((project) => (
                       <Link key={project.id} href={project.image} className="group">
                           <div className="relative overflow-hidden rounded-2xl lg:m-5   hover:border-gray-600 transition-all duration-300">
-                              <div className="relative sm:h-72 lg:h-96" style={{ height: project.hight }}>
+                              <div className="relative sm:h-72 lg:h-96" style={{ height: project.height }}>
                                   <Image
                                       src={`${project.image}`}
                                       alt={project.title}
